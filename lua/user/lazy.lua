8d6ed002988c8c6619f1a6d22aabfbda4c55fb33
Thoogs <thoogs@pm.me>
2023-08-01 20:48:29 +0200

    Add lsp to config

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -35,4 +35,19 @@ lazy.setup({
 -- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' }},
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  },
 })
